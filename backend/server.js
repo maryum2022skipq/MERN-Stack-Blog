a61bca8357b70151ef7b2561062c29289b34783c
Maryum <maryum.habib.sculptor@gmail.com>
2022-11-26 14:02:25 +0500

    backend completed routes setup 2

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv").config({
 });
 const cors = require("cors");
 const colors = require("colors");
+const multer = require("multer");
+const path = require("path")
 
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
@@ -12,6 +14,20 @@ connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "media")
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name)
+  }
+})
+
+const upload = multer({ storage: storage })
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded.")
+})
 
 app.use(cors());
 app.use(express.json());
